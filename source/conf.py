2cd9702938199ffb06d9c0e6e79b4d04b9cbfc37
saikrishnachaitanya24 <saikrishnachaitanya.annavazala@gmail.com>
2025-02-14 12:25:43 -0500

    Initial Commit

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SCP-Documentation'
+copyright = '2025, Sai Krishna Chaitanya Annavazala and Drew Parker'
+author = 'Sai Krishna Chaitanya Annavazala and Drew Parker'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
