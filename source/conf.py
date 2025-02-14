6253e7d990f0ddfdeca466a6fe844339c9196f8a
Sai Krishna Chaitanya Annavazala <106294457+SaiKrishnaChaitanya24@users.noreply.github.com>
2025-02-14 12:27:47 -0500

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -10,6 +10,7 @@ project = 'SCP-Documentation'
 copyright = '2025, Sai Krishna Chaitanya Annavazala and Drew Parker'
 author = 'Sai Krishna Chaitanya Annavazala and Drew Parker'
 release = '1.0.0'
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
